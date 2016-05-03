402c59d7a4c65240c659cca8bf55822b7cf94d90
frankpunx <francisco.jl.veiga@gmail.com>
2016-05-03 17:09:24 +0100

    gerador de tabuleiro feito

--- C# project/Pentago_Tests/Program.cs
+++ C# project/Pentago_Tests/Program.cs
@@ -4,9 +4,9 @@
     {
         //UnitTesting.testAlphaBeta();
         //UnitTesting.testHeuristicA();
-        UnitTesting.testHeuristicAGood(1000, Pentago_Rules.EvaluationFunction.controlHeuristic, 4, Pentago_Rules.EvaluationFunction.heuristicA, 4, UnitTesting.testSecond, false, false);
+        //UnitTesting.testHeuristicAGood(1000, Pentago_Rules.EvaluationFunction.controlHeuristic, 4, Pentago_Rules.EvaluationFunction.heuristicA, 4, UnitTesting.testSecond, false, false);
         //Pentago1P.play();
-
+        UnitTesting.testGenerateNewBoard();
         //UnitTesting.testMinMax();
         //UnitTesting.test_auxiliar_methods();
 
