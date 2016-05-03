b734a1b7de19e09761f299c0893011f0c2a8a149
frankpunx <francisco.jl.veiga@gmail.com>
2016-05-03 17:31:05 +0100

    correcções

--- C# project/Pentago_Tests/Program.cs
+++ C# project/Pentago_Tests/Program.cs
@@ -6,10 +6,10 @@
         //UnitTesting.testHeuristicA();
         //UnitTesting.testHeuristicAGood(1000, Pentago_Rules.EvaluationFunction.controlHeuristic, 4, Pentago_Rules.EvaluationFunction.heuristicA, 4, UnitTesting.testSecond, false, false);
         //Pentago1P.play();
-        UnitTesting.testGenerateNewBoard();
+
         //UnitTesting.testMinMax();
         //UnitTesting.test_auxiliar_methods();
-
+        UnitTesting.testGenerateNewBoard();
         //PentagoPandora.BUILD_PANDORA();
         //Console.WriteLine("---ENDED---")
     }
--- C# project/Pentago_Tests/UnitTests/UnitTesdting.testGenerateNewBoard.cs
+++ C# project/Pentago_Tests/UnitTests/UnitTesdting.testGenerateNewBoard.cs
@@ -5,7 +5,7 @@ static partial class UnitTesting
 {
     static public void testGenerateNewBoard()
     {
-        GenerateRandomBoard ranBoard = new GenerateRandomBoard(17, Pentago_GameBoard.blacks_turn);
+        GenerateRandomBoard ranBoard = new GenerateRandomBoard(17, Pentago_GameBoard.whites_turn);
         ranBoard.generateNewBoard();
         ranBoard.Pentago_gb.print_board();
         ranBoard.generateNewBoard();
